f8d0028ab9ee6de486024bbcd9c547eaec2ada8f
Fedotov Aleksey <far@rio-soft.ru>
2020-08-09 19:59:12 +0500

    type number

--- understanding_typescript/app.js
+++ understanding_typescript/app.js
@@ -1,1 +1,7 @@
-console.log('First code from ts');
+function add(num1, num2) {
+    return num1 + num2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log("result: " + result);
--- understanding_typescript/app.ts
+++ understanding_typescript/app.ts
@@ -1,1 +1,10 @@
-console.log('First code from ts');
+function add(num1: number, num2: number):number {
+	return num1 + num2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result:number = add(number1, number2);
+
+console.log(`result: ${result}`);
